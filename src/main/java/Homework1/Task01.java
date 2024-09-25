1abbef698ce05690557a5fa0436de16271ea437f
Petko Avgarski <pavgarski@gmail.com>
2024-09-25 22:45:37 +0300

    Initial commit

---
+++ src/main/java/Homework1/Task01.java
@@ -0,0 +1,17 @@
+package Homework1;
+import java.util.Scanner;
+
+public class Task01 {
+    public  static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter one integer ");
+        int a = in.nextInt();
+        System.out.print("Enter second integer ");
+        int b = in.nextInt();
+
+        System.out.println("The first integer is " + a);
+        System.out.println("The second integer is " + b);
+
+    }
+}
