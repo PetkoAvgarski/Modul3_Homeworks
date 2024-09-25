c881dd459cbcca37be301e13e2d90b07631d0539
Petko Avgarski <pavgarski@gmail.com>
2024-09-26 00:23:38 +0300

    final version of Task05 and Task07

---
+++ src/main/java/Homework1/Task05.java
@@ -0,0 +1,13 @@
+package Homework1;
+
+public class Task05 {
+    public static void main(String[] args){
+        for (int i = 1; i <= 100; i++) {
+            System.out.println( i );
+        }
+        System.out.println();
+        for (int i = 100; i > 0; i--) {
+            System.out.println( i );
+        }
+    }
+}
