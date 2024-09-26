bc84fd3da405b7cb13418f1f28522b9311d3345c
Petko Avgarski <pavgarski@gmail.com>
2024-09-27 00:26:52 +0300

    Upload Task06

--- src/main/java/Homework1/Task06.java
+++ src/main/java/Homework1/Task06.java
@@ -1,4 +1,37 @@
 package Homework1;
+import java.util.Scanner;
 
 public class Task06 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a number from 1 to 7 ");
+        int dayOfWeek = in.nextInt();
+        switch (dayOfWeek){
+            case 1 :
+                System.out.println("Monday");
+                break;
+            case 2 :
+                System.out.println("Tuesday");
+                break;
+            case 3 :
+                System.out.println("Wednesday");
+                break;
+            case 4 :
+                System.out.println("Thursday");
+                break;
+            case 5 :
+                System.out.println("Friday");
+                break;
+            case 6 :
+                System.out.println("Saturday");
+                break;
+            case 7 :
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("You have entered an invalid number");
+        }
+
+
+    }
 }
