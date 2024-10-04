8c759e0513ba8a9b63b192d73e30cafa81af355b
Petko Avgarski <pavgarski@gmail.com>
2024-10-04 23:45:10 +0300

    Upload Task02 / HW2

---
+++ src/main/java/Homework2/Task02.java
@@ -0,0 +1,10 @@
+package Homework2;
+
+public class Task02 {
+    static double myMethodArithmeticMeanOf2Numbers(double num1, double num2){
+        return (num1 + num2)/2;
+    }
+    public static void main(String[] args){
+        System.out.println(myMethodArithmeticMeanOf2Numbers(3.5 , 45.56));
+    }
+}
