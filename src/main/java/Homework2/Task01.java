62681a92f4792f98206025d8fc21ee6296f186ad
Petko Avgarski <pavgarski@gmail.com>
2024-10-04 23:33:17 +0300

    Upload Task01 / HW2

---
+++ src/main/java/Homework2/Task01.java
@@ -0,0 +1,10 @@
+package Homework2;
+
+public class Task01 {
+    static double myMethodSunOf2Numbers(double num1, double num2){
+        return num1 + num2;
+    }
+    public static void main(String[] args){
+        System.out.println(myMethodSunOf2Numbers(3.5 , 45.56));
+    }
+}
