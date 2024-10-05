d02b3d696936d64013dd3e46a2ddbc2bb6fb8b02
Petko Avgarski <pavgarski@gmail.com>
2024-10-05 18:06:26 +0300

    Upload Task03 / HW2

---
+++ src/main/java/Homework2/Task03.java
@@ -0,0 +1,10 @@
+package Homework2;
+
+public class Task03 {
+    public static void main(String[] args){
+       double[] doubleArray = {3.56, 57.89, 45.86, 116.45, 435.67};
+       for(int i = 0; i <doubleArray.length; i++){
+           System.out.println(doubleArray[i]);
+        }
+    }
+}
