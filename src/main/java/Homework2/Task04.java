e7b31e49e045488471bf0bbe0ccb37a9a844f3a2
Petko Avgarski <pavgarski@gmail.com>
2024-10-06 17:19:13 +0300

    Upload Task04 / HW2

--- src/main/java/Homework2/Task04.java
+++ src/main/java/Homework2/Task04.java
@@ -1,5 +1,16 @@
 package Homework2;
 
 public class Task04 {
+    public static void main(String[] args){
+        int[] intArray = {3, 56, 1983, 2017, 678, 34, 982, 396, 6, 495};
+        for (int i = 0; i < intArray.length; i++)
+            System.out.println(intArray[i]);
+
+        int num = 0;
+        while (num < intArray.length ){
+            System.out.print(intArray[num] + " ");
+            num ++;}
+
+    }
 
 }
