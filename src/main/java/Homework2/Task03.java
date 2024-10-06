fc284b64395abbc2a9af5e76be9c1fd94f7ca9e2
Petko Avgarski <pavgarski@gmail.com>
2024-10-06 12:44:18 +0300

    Update Task03 / HW2

--- src/main/java/Homework2/Task03.java
+++ src/main/java/Homework2/Task03.java
@@ -3,7 +3,7 @@ package Homework2;
 public class Task03 {
     public static void main(String[] args){
        double[] doubleArray = {3.56, 57.89, 45.86, 116.45, 435.67};
-       for(int i = 0; i <doubleArray.length; i++){
+       for (int i = 0; i <doubleArray.length; i++){
            System.out.println(doubleArray[i]);
         }
     }
