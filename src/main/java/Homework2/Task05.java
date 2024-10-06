465c6c179c02a6b3714597b8248768aa4ca38660
Petko Avgarski <pavgarski@gmail.com>
2024-10-06 23:46:57 +0300

    Upload Task07 / HW2

---
+++ src/main/java/Homework2/Task05.java
@@ -0,0 +1,13 @@
+package Homework2;
+
+public class Task05 {
+    public static void main(String[] args){
+        int[] intArray = {10, 12, 1, 8, 4};
+        int[] newIntArray = new int[intArray.length];
+        System.out.print("Contents of newIntArray[] are: ");
+        for (int i = 0; i < intArray.length; i++) {
+            newIntArray[i] = intArray[i];
+            System.out.print(newIntArray[i] + "  ");
+        }
+    }
+}
