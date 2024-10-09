11b3490b7a775e0e4a8bacbbef2343adeccf578e
Petko Avgarski <pavgarski@gmail.com>
2024-10-09 19:49:29 +0300

    Update Task05 / HW2

--- src/main/java/Homework2/Task05.java
+++ src/main/java/Homework2/Task05.java
@@ -1,7 +1,7 @@
 package Homework2;
 
 public class Task05 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int[] intArray = {10, 12, 1, 8, 4};
         int[] newIntArray = new int[intArray.length];
         System.out.print("Contents of newIntArray[] are: ");
